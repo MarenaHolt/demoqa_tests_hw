8215bf9d03fc5594f5cd4d5b29ce936b283a906f
Marena <fabriana.sands@gmail.com>
2023-06-13 19:48:29 +0500

    add gitignore

--- src/test/java/com/demoqa/tests/PracticeFormTests.java
+++ src/test/java/com/demoqa/tests/PracticeFormTests.java
@@ -17,7 +17,7 @@ public class PracticeFormTests {
     static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
-        Configuration.holdBrowserOpen = true;
+        //Configuration.holdBrowserOpen = true;
         Configuration.pageLoadStrategy = "eager";
     }
 
