6e3b5cc9b6f0d5280829f84e19fff37f742322ec
Marena <fabriana.sands@gmail.com>
2023-07-16 19:02:31 +0500

    add allure

---
+++ src/test/java/com/demoqa/tests/test_with_allure/PracticeFormTestWithFakerHWTestsWithAllure.java
@@ -0,0 +1,65 @@
+package com.demoqa.tests.test_with_allure;
+
+import com.demoqa.pages.RegistrationPage;
+import com.demoqa.tests.TestBase;
+import com.demoqa.utils.RandomUtils;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+
+public class PracticeFormTestWithFakerHWTestsWithAllure extends TestBase {
+
+    RegistrationPage registrationPage = new RegistrationPage();
+
+    RandomUtils randomUtils = new RandomUtils();
+    String name = randomUtils.getFirstName(),
+            lastName = randomUtils.getLastName(),
+            email = randomUtils.getEmail(),
+            gender = randomUtils.getRandomGender(),
+            phone = randomUtils.getPhone(),
+            hobby = randomUtils.getRandomHobbies(),
+            image = "image.img",
+            address = randomUtils.getAddress(),
+            state = randomUtils.getRandomState(),
+            city = randomUtils.getRandomCity(state),
+            month = randomUtils.getRandomMonth(),
+            day = randomUtils.getDay(month),
+            year = randomUtils.getYear(),
+            subject = randomUtils.getSubjects();
+
+
+    @Test
+    @Tag("simple")
+    @DisplayName("Successful registration")
+    void successTest() {
+
+        registrationPage.openPage()
+                .setFirstName(name)
+                .setLastName(lastName)
+                .setUserEmail(email)
+                .setGender(gender)
+                .setPhoneNumber(phone)
+                .setBirthDay(day, month, year)
+                .setSubjects(subject)
+                .setHobbies(hobby)
+                .setUploadPicture("img/"+ image)
+                .setCurrentAddress(address)
+                .setStateAndCity(state, city)
+                .clickSubmit();
+
+        registrationPage.verifyTitle("Thanks for submitting the form")
+                .verifyResult("Student Name", name + " " + lastName)
+                .verifyResult("Student Email", email)
+                .verifyResult("Gender", gender)
+                .verifyResult("Mobile", phone)
+                .verifyResult("Date of Birth", day + " " + month + "," + year)
+                .verifyResult("Subjects", subject)
+                .verifyResult("Hobbies", hobby)
+                .verifyResult("Picture", image)
+                .verifyResult("Address", address)
+                .verifyResult("State and City", state + " " + city);
+    }
+
+
+}
---
+++ src/test/java/com/demoqa/tests/test_with_allure/TextboxWithFakerHwTestWithAllure.java
@@ -0,0 +1,36 @@
+package com.demoqa.tests.test_with_allure;
+
+import com.demoqa.pages.RegistrationPage;
+import com.demoqa.pages.TextBoxPage;
+import com.demoqa.tests.TestBase;
+import com.demoqa.utils.RandomUtils;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+public class TextboxWithFakerHwTestWithAllure extends TestBase {
+
+    TextBoxPage textBoxPage = new TextBoxPage();
+    RegistrationPage registrationPage = new RegistrationPage();
+    RandomUtils randomUtils = new RandomUtils();
+
+    String name = randomUtils.getFirstName(),
+            email = randomUtils.getEmail(),
+            currentAddress = randomUtils.getAddress(),
+            permanentAddress = randomUtils.getAddress();
+
+    @Test
+    @Tag("simple")
+    @DisplayName("Successful text BoxPage")
+    void successTest() {
+        textBoxPage.openTextBoxPage()
+                .setName(name)
+                .setEmail(email)
+                .setCurrentAddress(currentAddress)
+                .setPermanentAddress(permanentAddress);
+
+        registrationPage.clickSubmit();
+
+        textBoxPage.verifyResult(name, email, currentAddress, permanentAddress);
+    }
+}
